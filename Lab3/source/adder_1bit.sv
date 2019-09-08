8c67059d65a0f75e30e6c0a8c4063bf125ef12e5
David Dang <dangd@purdue.edu>
2019-09-08 00:29:27 -0400

    adder_1bit.sv

--- Lab3/source/adder_1bit.sv
+++ Lab3/source/adder_1bit.sv
@@ -15,7 +15,21 @@ module adder_1bit
 	output wire carry_out
 );
 
-assign sum = carry_in ^ (a ^ b);
-assign carry_out = ((!carry_in) & b & a) | (carry_in & (b | a));
+	always @ (a, b) begin
+
+		assert((a == 1'b1) || (a == 1'b0))
+		else $error("Input 'a' of component is not a digital logic value");
+
+		assert((b == 1'b1) || (b == 1'b0))
+		else $error("Input 'b' of component is not a digital logic value");
+
+		assert((carry_in == 1'b1) || (carry_in == 1'b0))
+		else $error("carry_in is incorrect");
+
+	end
+
+	assign sum = carry_in ^ (a ^ b);
+	assign carry_out = ((!carry_in) & b & a) | (carry_in & (b | a));
+
 
 endmodule 
